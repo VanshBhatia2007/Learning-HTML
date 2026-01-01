655351f04b42fda8393b56afd5474c9f1383c872
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-01-01 22:16:41 +0530

    added new topics

--- Backend/EJS/script.js
+++ Backend/EJS/script.js
@@ -11,8 +11,27 @@ app.get("/",(req,res)=>{
     res.render("home.ejs");
 });
 
-app.get("/hello",(req,res)=>{
-    res.send("hello"); //render used for sending a htm or ejs file
+// app.get("/hello",(req,res)=>{
+//     res.send("hello"); //render used for sending a htm or ejs file
+// });
+
+app.get("/dice",(req,res)=>{
+    let num = Math.floor(Math.random()*6)+1;
+    res.render("dice.ejs",{dice:num});// dice is key in ejs file and num is value for that dice
+});
+
+app.get("/ig/:username",(req,res)=>{
+    // const followers=[1,2,3,4];
+    let instadata =require("./data.json");
+    let {username} = req.params;
+    const data= instadata[username];
+    if(data){
+        res.render("insta.ejs",{data});
+    }
+    else{
+        res.render("error.ejs")
+    }
+    
 });
 
 app.listen(port,()=>{
