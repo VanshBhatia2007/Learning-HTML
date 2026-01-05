94db37cacee89641b889d48c010ac5cc0849a04c
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-01-05 20:55:34 +0530

    added new topics

--- Backend/Rest/index.js
+++ Backend/Rest/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4 : uuidv4 } = require('uuid');
+var methodOverride = require("method-override");
+
+// uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 const port = 3000;
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
@@ -12,17 +16,17 @@ let posts=[
     {
         username:"vansh bhatia",
         content:"i can do this all day",
-        id:"1a",
+        id: uuidv4(),
     },
     {
         username:"vansh bhatia",
         content:"i can do this all day",
-        id:"2a",
+        id: uuidv4(),
     },
     {
         username:"vansh bhatia",
         content:"i can do this all day",
-        id:"3a",
+        id: uuidv4(),
     },
 ];
 
@@ -37,7 +41,8 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let {username,content} = req.body;
-    posts.push({username,content});
+    let id = uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
      
 });
@@ -47,6 +52,27 @@ app.get("/posts/:id",(req,res)=>{
    let post= posts.find((p) => id === p.id);
    res.render("show.ejs",{post});
 });
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let newcontent = req.body.content;
+    let post= posts.find((p) => id === p.id);
+    post.content= newcontent;
+    res.redirect("/posts");
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts= posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post= posts.find((p) => id === p.id);
+    res.render("edit-post.ejs",{post});
+});
+
 
 app.listen(port,()=>{
     console.log(`app is listening ${port}`);
