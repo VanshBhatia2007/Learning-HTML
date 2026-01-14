cc8a6e8b1d98256e12eb55076e58b19f50e8b1a8
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-01-15 00:38:48 +0530

    added new topics

--- Backend/databases/Mongodb/index.js
+++ Backend/databases/Mongodb/index.js
@@ -10,3 +10,25 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 }
+
+const userschema = new mongoose.Schema({
+    name:String,
+    email: String,
+    age: Number
+});
+
+const User = mongoose.model("User",userschema);
+
+const user1 = new User({
+    name: "Vansh",
+    email: "abc",
+    age: 18
+});
+
+user1.save()
+            .then((res)=>{
+                console.log(res);
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
