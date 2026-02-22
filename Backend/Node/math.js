cc6baadd2198c9e6c4d7ffffd33a4e21cdc4e9fe
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-02-22 19:13:56 +0530

    added backend

---
+++ Backend/Node/math.js
@@ -0,0 +1,7 @@
+let sum = (a,b) => a+b;
+let mul = (a,b)=> a*b; // exports.mul
+
+module.exports={
+    sum:sum,
+    mul:mul,
+};
---
+++ Backend/Node/script.js
@@ -0,0 +1,4 @@
+let a=10;
+const maths = require("./math");
+
+console.log(maths.sum(2,2));
