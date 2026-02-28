ecc6e43c015b9053d5356d59829eee2ba77c238b
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-03-01 01:47:45 +0530

    added new topics

--- Backend/Rest/index.js
+++ Backend/Rest/index.js
@@ -1,15 +1,51 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port =3000;
+const port = 3000;
 app.use(express.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 
-app.get("/",(req,res)=>{
-    res.send("server is working");
+let posts=[
+    {
+        username:"vansh bhatia",
+        content:"i can do this all day",
+        id:"1a",
+    },
+    {
+        username:"vansh bhatia",
+        content:"i can do this all day",
+        id:"2a",
+    },
+    {
+        username:"vansh bhatia",
+        content:"i can do this all day",
+        id:"3a",
+    },
+];
+
+
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+});
+app.get("/posts/new",(req,res)=>{
+    res.render("form.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    let {username,content} = req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
+     
+});
+
+app.get("/posts/:id",(req,res)=>{
+   let {id}=req.params;
+   let post= posts.find((p) => id === p.id);
+   res.render("show.ejs",{post});
 });
 
 app.listen(port,()=>{
